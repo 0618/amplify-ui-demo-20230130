d66c79cd72d1c2be60e7367990c03f4597f89709
MJ☔ <zhamujun@amazon.com>
2023-01-30 13:19:16 -0800

    add Amplify Auth and Geo

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyuidemo2023013a25bc9e7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "geo": {
+    "mapa840e797": {
+      "Arn": "string",
+      "Name": "string",
+      "Region": "string",
+      "Style": "string"
+    }
+  }
+}
